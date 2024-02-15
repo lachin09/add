86173c8ae875a04e3c5be1df47d326142d6f9445
lachin <kavkazes09@gmail.com>
2024-02-16 01:41:15 +0200

    was adding monrope fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,88 +12,35 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.blue,
-        body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.arrow_back_ios),
-                    color: Colors.amber,
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.search_rounded,
-                      color: Colors.amber,
-                      size: 30,
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 0,
-              ),
-              const Text(
-                'To do list.',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 25,
-                  color: Colors.white,
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 390,
-                    height: 600,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(23),
-                      color: Colors.amber,
-                    ),
-                    child: const Column(
-                      children: [
-                        Icon(
-                          Icons.remove_red_eye,
-                        ),
-                        Text(
-                          'wakeup to Pray',
-                          style: TextStyle(
-                              fontSize: 25,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.blue),
-                        ),
-                        Text(
-                          'read a Kuran.',
-                          style: TextStyle(
-                              fontSize: 25,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.blue),
-                        ),
-                        Text(
-                          'go to work',
-                          style: TextStyle(
-                              fontSize: 25,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.blue),
-                        )
-                      ],
-                    ),
-                  ),
-                ],
-              )
-            ],
-          ),
+        backgroundColor: Colors.white,
+        body: Row(
+          children: [Text('wallet')],
         ),
       ),
+    );
+  }
+}
+
+class StatussBar extends StatefulWidget {
+  const StatussBar({super.key});
+
+  @override
+  _StatussBarState createState() => _StatussBarState();
+}
+
+class _StatussBarState extends State {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Row(children: [
+        Text('WALLET'),
+        Icon(
+          Icons.settings,
+        )
+      ]),
     );
   }
 }
